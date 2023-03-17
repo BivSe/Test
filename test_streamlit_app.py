9a8c603c04dd6111e6c8632b26d1909ab7e0cc40
BivSe <127946889+BivSe@users.noreply.github.com>
2023-03-17 16:25:54 +0500

    Update test_streamlit_app.py

--- test_streamlit_app.py
+++ test_streamlit_app.py
@@ -8,5 +8,5 @@ client = TestClient(app)
 
 def test_read_main():
     response = client.get("/")
-    assert response.status_code == 200
+    assert response.status_code == 422
     assert response.json() == {"Key": "True"}
